8d841bef8c252dcc8c6d7003e03710156d768b5f
Aleksandar <acostojanovic22@gmail.com>
2024-07-27 02:54:39 +0200

    Database User

---
+++ ASP.Net/MyBookShelfBackend/Data/UserContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MyBookShelfBackend.Models;
+
+namespace MyBookShelfBackend.Data
+{
+    public class UserContext : DbContext
+    {
+        public UserContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.HasIndex(user => user.Email).IsUnique();
+            });
+        }
+    }
+}
--- ASP.Net/MyBookShelfBackend/obj/Debug/net8.0/MyBookShelfBackend.AssemblyInfo.cs
+++ ASP.Net/MyBookShelfBackend/obj/Debug/net8.0/MyBookShelfBackend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyBookShelfBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5249ef7b9ec86a89f3d8b6b745ca2d472c863349")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyBookShelfBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyBookShelfBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
